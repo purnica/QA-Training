95624a643cfdc872a81ffc1ad36d7e18db101bae
purnica <prajapatipurnika26@gmail.com>
2026-06-30 16:52:59 +0545

    locators

--- Selenium/firstcalss.py
+++ Selenium/firstcalss.py
@@ -14,17 +14,34 @@ url = "https://www.saucedemo.com/"
 
 driver.get(url)
 
-uname = driver.find_element(By.ID,"user-name")
+# using locator ID
+# uname = driver.find_element(By.ID,"user-name")
+
+# using locatorrelative path
+uname = driver.find_element(By.XPATH,"//*[@id='user-name']")
 uname.send_keys("standard_user")
 
-password = driver.find_element(By.ID,"password")
+# password = driver.find_element(By.ID,"password")
+
+password = driver.find_element(By.XPATH,"//*[@id='password']")
 password.send_keys("secret_sauce")
 
-loginbtn = driver.find_element(By.ID,"login-button")
+# loginbtn = driver.find_element(By.ID,"login-button")
+
+loginbtn = driver.find_element(By.XPATH,"//*[@id='login-button']")
+
 loginbtn.click()
 
 # execute delay
 time.sleep(3)
+
+# assert "inventory" in driver.current_url, "Login Unsuccessful"
+
+if "inventory" in driver.current_url:
+    print ("Login successful")
+
+else:
+    print ("Login Unsuccessful")
 
 # driver close
 driver.quit()
